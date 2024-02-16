ef6d98a5018d0df300e0555a21441572d90b95f5
HasibGit <hasibullah04@gmail.com>
2024-02-17 00:33:55 +0600

    gql crud implemented that commuticates with gql endpoint

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,127 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Apollo, gql } from 'apollo-angular';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'apollo-client';
+export class AppComponent implements OnInit {
+  rates: any[] = [];
+  loading = true;
+  error: any;
+
+  constructor(private apollo: Apollo) {}
+
+  ngOnInit() {
+    // this.addRecipe('mutton teheri', 'very tasty 😋');
+
+    this.getAllRecipes();
+
+    // this.getRecipe();
+
+    //this.editRecipe('65cd07d614cb755a158ec4b6', 'Beef Khicuri', 'Pretty good');
+
+    // this.deleteRecipe();
+  }
+
+  addRecipe(name: string, description: string) {
+    this.apollo
+      .mutate({
+        mutation: gql`
+          mutation Mutation($recipeInput: RecipeInput) {
+            createRecipe(recipeInput: $recipeInput) {
+              createdAt
+              description
+              name
+              thumbsDown
+              thumbsUp
+            }
+          }
+        `,
+        variables: {
+          recipeInput: {
+            name: name,
+            description: description,
+          },
+        },
+      })
+      .subscribe((res) => console.log(res));
+  }
+
+  editRecipe(id: string, name: string, description: string) {
+    this.apollo
+      .mutate({
+        mutation: gql`
+          mutation EditRecipe($id: ID!, $recipeInput: RecipeInput) {
+            editRecipe(ID: $id, recipeInput: $recipeInput)
+          }
+        `,
+        variables: {
+          id: id,
+          recipeInput: {
+            description: description,
+            name: name,
+          },
+        },
+      })
+      .subscribe((res) => console.log(res));
+  }
+
+  getAllRecipes() {
+    this.apollo
+      .watchQuery({
+        query: gql`
+          query GetRecipes($amount: Int) {
+            getRecipes(amount: $amount) {
+              createdAt
+              description
+              name
+              thumbsDown
+              thumbsUp
+            }
+          }
+        `,
+        variables: {
+          amount: 10, // Specify the amount you want to retrieve
+        },
+      })
+      .valueChanges.subscribe((res) => console.log(res));
+  }
+
+  getRecipe() {
+    this.apollo
+      .watchQuery({
+        query: gql`
+          query Recipe($id: ID!) {
+            recipe(ID: $id) {
+              createdAt
+              description
+              name
+              thumbsDown
+              thumbsUp
+            }
+          }
+        `,
+        variables: {
+          id: '65cd07d614cb755a158ec4b6',
+        },
+      })
+      .valueChanges.subscribe((res) => console.log(res));
+  }
+
+  deleteRecipe() {
+    this.apollo
+      .mutate({
+        mutation: gql`
+          mutation Mutation($deleteRecipeId2: ID!) {
+            deleteRecipe(ID: $deleteRecipeId2)
+          }
+        `,
+        variables: {
+          deleteRecipeId2: '65cd07d614cb755a158ec4b6',
+        },
+      })
+      .subscribe((res) => console.log(res));
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { APOLLO_OPTIONS, ApolloModule } from 'apollo-angular';
+import { HttpLink } from 'apollo-angular/http';
+import { HttpClientModule } from '@angular/common/http';
+import { InMemoryCache } from '@apollo/client/core';
+
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, ApolloModule, HttpClientModule],
+  providers: [
+    {
+      provide: APOLLO_OPTIONS,
+      useFactory(httpLink: HttpLink) {
+        return {
+          cache: new InMemoryCache(),
+          link: httpLink.create({
+            uri: 'http://localhost:5000',
+          }),
+        };
+      },
+      deps: [HttpLink],
+    },
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {
+  /**
+   *
+   */
+  constructor() {}
+}
